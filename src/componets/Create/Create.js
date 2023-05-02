642f95193be4ba8842ac9ecf17937a89ae5912b9
Genesis Olivero <genesis2833@gmail.com>
2023-05-02 19:54:16 -0400

    creacion del form de pokemon

--- src/componets/Create/Create.js
+++ src/componets/Create/Create.js
@@ -1,9 +1,34 @@
-
+import "./Create.css"
 
 const Create = () =>{
     return(
         <div>
-            
+           <h1>Create tu pokemon</h1>
+
+
+		
+        <form action="enviar.php" method="post">
+		<label for="nombre">Nombre:</label>
+		<input type="text" id="nombre" name="nombre" placeholder="Nombre" required/>
+
+		<label for="correo">Descripcion</label>
+		<input type="email" id="Descripcion" name="descripcion"  placeholder="Descripcion" required/>
+
+		<label for="asunto">Poderes</label>
+		<input type="text" id="poderes" name="poderes" placeholder="Poderes" required/>
+
+        <div className="archivo">
+        <label for="correo">Seleccionar archivo:</label>
+		<input type="file" id="archivo" name="archivo" required/>
+        </div>
+        <div>
+		<input type="submit" value="Enviar"/>
+        </div>
+	</form>
+	
+
+
+
         </div>
     )
 }
