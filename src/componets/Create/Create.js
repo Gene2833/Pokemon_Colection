c440a2da5d8a8d0e8b5e40f91f59eb1274b8ac03
Genesis Olivero <genesis2833@gmail.com>
2023-05-10 14:24:22 -0400

    modificaciones finales

--- src/componets/Create/Create.js
+++ src/componets/Create/Create.js
@@ -1,36 +1,84 @@
 import "./Create.css"
+import { useState } from "react";
+
 
 const Create = () =>{
-    return(
-        <div>
-           <h1>Create tu pokemon</h1>
+    const [tarjetas, setTarjetas] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [poderes, setPoderes] = useState('');
+    const [imagen, setImagen] = useState(null);
+    
+ const handleImagenChange = (event) => {
+            setImagen(event.target.files[0]);
+          };
+
+    const guardarTarjetas = (e) =>{
+        e.preventDefault()
+      
+       
+          // Crea un nuevo objeto de tarjeta
+          const nuevaTarjeta = {
+            nombre: nombre,
+            descripcion: descripcion,
+            poderes: poderes,
+            imagen:imagen
+          };
+
+    // Agrega la nueva tarjeta al vector
+    setTarjetas([...tarjetas, nuevaTarjeta]);
+
+    // Limpia los campos del formulario
+    setNombre('');
+    setDescripcion('');
+    setPoderes('');
+    setImagen(null);
+  };
+
+  return(
+    <div>
+       <h1>Create tu pokemon</h1>
 
 
-		
-        <form action="enviar.php" method="post">
-		<label for="nombre">Nombre:</label>
-		<input type="text" id="nombre" name="nombre" placeholder="Nombre" required/>
+    
+    <form onSubmit={guardarTarjetas}>
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" placeholder="Nombre" onChange={(e) => setNombre(e.target.value)} required/>
 
-		<label for="correo">Descripcion</label>
-		<input type="email" id="Descripcion" name="descripcion"  placeholder="Descripcion" required/>
+    <label for="correo">Descripcion</label>
+    <input type="text" id="Descripcion" name="descripcion"  placeholder="Descripcion" onChange={(e) => setDescripcion(e.target.value)} required />
 
-		<label for="asunto">Poderes</label>
-		<input type="text" id="poderes" name="poderes" placeholder="Poderes" required/>
+    <label for="asunto">Poderes</label>
+    <input type="text" id="poderes" name="poderes" placeholder="Poderes" onChange={(e) => setPoderes(e.target.value)}required />
 
-        <div className="archivo">
-        <label for="correo">Seleccionar archivo:</label>
-		<input type="file" id="archivo" name="archivo" required/>
+    <div className="archivo">
+    <label for="correo">Seleccionar archivo:</label>
+    <input type="file" id="archivo" name="archivo" onChange={handleImagenChange} required/>
+    </div>
+
+    <input type="submit" value="Enviar"/>
+
+</form>
+
+    {tarjetas.map((tarjeta, index) => (
+        <div className="contenedor" key={index}>
+            <h2>{tarjeta.nombre}</h2>
+            <p>{tarjeta.descripcion}</p>
+            <h6>{tarjeta.poderes}</h6>
+            <img src={URL.createObjectURL(tarjeta.imagen)} alt="" />
         </div>
-        <div>
-		<input type="submit" value="Enviar"/>
-        </div>
-	</form>
-	
+      ))}
 
+    </div>
 
-
-        </div>
-    )
+)  
+    
 }
 
+
+      
+
+
+
 export default Create
+
--- src/componets/Footer/Footer.js
+++ src/componets/Footer/Footer.js
@@ -9,11 +9,6 @@ const Footer = () =>{
           <div className="logo">
           <Link to="/" class="navbar-brand" href="#"> <img  src="https://tcg.pokemon.com/assets/img/global/logos/es-es/tcg-logo.png"  alt="pokemon" /></Link>
           </div>
-          <ul className="social-icons">
-          <li><Link to={`#`}> <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/instagram_black_logo_icon_147122.png" style={{width:60, height:60}} alt="instagram"/></Link></li>
-          <li><Link to={`#`}> <img src="https://cdn.icon-icons.com/icons2/1/PNG/256/social_facebook_fb_35.png" style={{width:60, height:60}}  alt="facebbok"/></Link></li>
-          <li><Link to={`#`}> <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/whatsapp_black_logo_icon_147050.png" style={{width:60, height:60}}  alt="whatsapp"/></Link></li>       
-        </ul>
         </div>
         <div className="bottom-bar">
           <p>&copy; 2023 Todos los derechos reservados</p>
